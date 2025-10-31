f28892af8c1b50239962d04d352917ce1ddd8d0d
ImanSTTT <iman.agung97@gmail.com>
2025-10-31 19:25:56 +0700

    firebase.ts

--- firebase.ts
+++ firebase.ts
@@ -1,18 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyD-T4qyu5lzI04aBeI_w6gsTHSxfaaemdM",
   authDomain: "audit-6214b.firebaseapp.com",
   projectId: "audit-6214b",
   storageBucket: "audit-6214b.firebasestorage.app",
   messagingSenderId: "425177114300",
-  appId: "1:425177114300:web:9a47b2f09cb0ee9a8d52b9",
-  measurementId: "G-KPPQBSLW4B"
+  appId: "1:425177114300:web:7404467a654c69528d52b9",
+  measurementId: "G-6M54N114K5"
 };
 
-// Inisialisasi Firebase
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Inisialisasi Cloud Firestore dan ekspor
-export const db = getFirestore(app);
+const analytics = getAnalytics(app);
